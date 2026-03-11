4ceaf77527c8a5c1a1474eda04aea2b65d637a60
rizkychandra <rizkychandra2204@gmail.com>
2026-03-11 08:17:45 +0700

    delete support cloudinary

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,3 @@ Route::get('/view/cv/rizky-chandra-khusuma', function () {
 
 // Redirect User Login
 Route::redirect('/user', '/dashboard/user/login');
-
-// Cloudinary Debug & Test Routes
-// Note: Keep these accessible for production debugging. Remove after fixing uploads.
-Route::post('/api/cloudinary-test', [\App\Http\Controllers\CloudinaryTestController::class, 'test'])->name('cloudinary-test');
-Route::get('/api/cloudinary-info', [\App\Http\Controllers\CloudinaryTestController::class, 'info'])->name('cloudinary-info');
